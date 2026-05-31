fb577889d3ed09900d0cbd960b584e28734878c5
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-05-31 22:22:06 +0600

    add room page added

---
+++ src/app/(main)/add-room /page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AddRoomWrapper from "../../../sections/Rooms/AddRoomWrapper";
+
+export const metadata = {
+  title: "Add Room - StudyNook",
+};
+const AddRoomPage = () => {
+  return (
+    <div>
+      <AddRoomWrapper></AddRoomWrapper>
+    </div>
+  );
+};
+
+export default AddRoomPage;
