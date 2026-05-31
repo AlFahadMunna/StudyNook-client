c4ad5db9ed7fd8c76e848b6b35033d9e06cb7738
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-05-31 15:14:56 +0600

    register page added

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import RegisterWrapper from "../../../components/auth/RegisterWrapper";
+
+export const metadata = {
+  title: "Register - StudyNook",
+};
+const Register = () => {
+  return (
+    <div>
+      <RegisterWrapper></RegisterWrapper>
+    </div>
+  );
+};
+
+export default Register;
