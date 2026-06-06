7f3f2b500fdaeaa3fa6e94cfc7aac78e2195e545
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-06-06 21:46:55 +0600

    get listing room logic updated

--- src/app/(main)/my-listing/page.jsx
+++ src/app/(main)/my-listing/page.jsx
@@ -7,18 +7,36 @@ export const metadata = {
 };
 
 const getListingRooms = async () => {
-  const { token } = await auth.api.getToken({
-    headers: await headers(),
-  });
+  try {
+    const { token } = await auth.api.getToken({
+      headers: await headers(),
+    });
 
-  const res = await fetch(process.env.NEXT_PUBLIC_URL + "/my-listing", {
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/my-listing`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      cache: "no-store",
+    });
 
-  const data = await res.json();
-  return data.data;
+    // FIRST check response
+    if (!res.ok) {
+      const text = await res.text();
+
+      console.log("SERVER ERROR:", text);
+
+      return [];
+    }
+
+    // only parse json if success
+    const data = await res.json();
+
+    return data.data;
+  } catch (error) {
+    console.log(error);
+
+    return [];
+  }
 };
 
 const MyListing = async () => {
